affed5d1add15113f7586f7b47c5e3747325bb30
Jerrares <136241881+Jerrares@users.noreply.github.com>
2024-04-06 18:25:14 +0200

    Update Site.py

--- flask/Site.py
+++ flask/Site.py
@@ -25,7 +25,7 @@ def nouveau_salon():										  # La fonction commence par récupérer le nom du
     if nom_salon in Liste_salons:                                                               # si le salon existe deja le site renvoie un message d'erreur "veuillez donner un autre nom".
         return render_template('ajouter_salon.html', message ="ce salon existe déja, veuillez donner un autre nom")
     nouveau_salon = pd.DataFrame([[nom_salon]], columns=['Test'] )
-    Liste_salons = pd.concat([Liste_salons,nouveau_salon], ignore_index=True)               # ce code permet d'ajouter un nouveau salon à une liste de salons stockée dans un fichier CSV, 
+    Liste_salons = pd.concat([Liste_salons,nouveau_salon], ignore_index=False)               # ce code permet d'ajouter un nouveau salon à une liste de salons stockée dans un fichier CSV, 
     Liste_salons.to_csv('Salons.csv')
     vide = pd.DataFrame({"Test":[]})# de créer un fichier CSV spécifique pour le nouveau salon,
     vide.to_csv(f'salons/{nom_salon}.csv', index=False)				  # et d'informer l'utilisateur que l'opération a réussi 
@@ -38,9 +38,13 @@ routes dynamiques en dessous
 
 @app.route('/<ligne>')
 def userlist(ligne):
-	Liste_user = pd.read_csv(f'salons/{ligne}.csv')		 			  # Cette ligne utilise la bibliothèque Pandas pour lire un fichier CSV situé dans le dossier salons
-	user = Liste_user['Test']								  # Cette ligne extrait une colonne spécifique du DataFrame Pandas créé à partir du fichier CSV
-	return render_template('page_utilisateurs.html', data=user)				  # Cette ligne génère une réponse HTML à partir du template
+    Liste_user = pd.read_csv(f'salons/{ligne}.csv')
+    adresses_ip = request.remote_addr
+    utilisateurs = pd.DataFrame([[adresses_ip]], columns=['Test'] )
+    Liste_user = pd.concat([Liste_user,utilisateurs], ignore_index=False)               # ce code permet d'ajouter un nouveau salon à une liste de salons stockée dans un fichier CSV, 
+    Liste_user.to_csv(f'{ligne}.csv')    		 			  # Cette ligne utilise la bibliothèque Pandas pour lire un fichier CSV situé dans le dossier salons
+    user = Liste_user['Test']								  # Cette ligne extrait une colonne spécifique du DataFrame Pandas créé à partir du fichier CSV
+    return render_template('page_utilisateurs.html', data=user)				  # Cette ligne génère une réponse HTML à partir du template
 
 @app.errorhandler(404)
 def page_not_found():										  # ici la fonction permet de gérer les erreurs 404 
@@ -49,3 +53,5 @@ def page_not_found():										  # ici la fonction permet de gérer les erreurs 
 @app.errorhandler(500)
 def cttcasé(error):
     return render_template('cassé.html')
+
+
